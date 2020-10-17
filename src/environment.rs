b5b07bde1d9d89eb07964ef4ee26d7b5a154d017
kyle.coffey <kyle.coffey@tamu.edu>
2020-10-16 19:05:39 -0500

    Environment semantics for blocks, blocks not supported yet

--- src/environment.rs
+++ src/environment.rs
@@ -1,14 +1,23 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, sync::RwLock};
 
 use crate::{object::Object, runtime_error::RuntimeError, token::Token};
 
 pub struct Environment {
+    enclosing: Option<Arc<RwLock<Environment>>>,
     values: HashMap<String, Arc<Object>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Self {
+            enclosing: None,
+            values: HashMap::new(),
+        }
+    }
+
+    pub fn new_enclosed(enclosing: Arc<RwLock<Environment>>) -> Self {
+        Self {
+            enclosing: Some(enclosing),
             values: HashMap::new(),
         }
     }
@@ -17,27 +26,40 @@ impl Environment {
         self.values.insert(name.to_owned(), value);
     }
 
+    fn try_get(&self, name: &Token) -> Option<Arc<Object>> {
+        self.values.get(&name.lexeme).cloned().or_else(|| {
+            self.enclosing
+                .as_ref()
+                .and_then(|e| e.write().unwrap().try_get(name))
+        })
+    }
+
     pub fn get(&self, name: &Token) -> Result<Arc<Object>, RuntimeError> {
-        if let Some(value) = self.values.get(&name.lexeme) {
-            Ok(value.clone())
-        } else {
-            Err(RuntimeError::new(
+        self.try_get(name).ok_or_else(|| {
+            RuntimeError::new(
                 name.clone(),
                 format!("Undefined variable '{}'.", name.lexeme),
-            ))
-        }
+            )
+        })
+    }
+
+    fn try_assign(&mut self, name: &Token, value: Arc<Object>) -> Option<()> {
+        self.values
+            .get_mut(&name.lexeme)
+            .map(|v| *v = value.clone())
+            .or_else(|| {
+                self.enclosing
+                    .as_ref()
+                    .and_then(|e| e.write().unwrap().try_assign(name, value))
+            })
     }
 
     pub fn assign(&mut self, name: &Token, value: Arc<Object>) -> Result<(), RuntimeError> {
-        match self.values.get_mut(&name.lexeme) {
-            Some(v) => {
-                *v = value;
-                Ok(())
-            }
-            None => Err(RuntimeError::new(
+        self.try_assign(name, value).ok_or_else(|| {
+            RuntimeError::new(
                 name.clone(),
                 format!("Undefined variable '{}'.", name.lexeme),
-            )),
-        }
+            )
+        })
     }
 }
